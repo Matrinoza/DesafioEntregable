8fcb11a4baf56fa68fd00326a8104370bb0d127b
Matrinoza <104370608+Matrinoza@users.noreply.github.com>
2024-10-02 21:22:23 -0300

    Update UsuarioNegocio.cs

--- SistemaGestionEntities1/Clases Estáticas/UsuarioNegocio.cs
+++ SistemaGestionEntities1/Clases Estáticas/UsuarioNegocio.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public static class UsuarioNeogcio
+{
+    public static List<Usuario> ObtenerTodosLosUsuarios()
+    {
+        return UsuarioData.ObtenerUsuarios();
+    }
+    public static void AgregarUsuario(Usuario usuario)
+    {
+        UsuarioData.AgregarUsuario(usuario);
+    }
+    public static void ActualizarUsuario(Usuario usuario)
+    {
+        UsuarioData.ActualizarUsuario(usuario);
+    }
+    public static void EliminarUsuario(int id)
+    {
+        UsuarioData.EliminarUsuario(id);
+    }
+}
